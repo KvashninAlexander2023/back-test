dcbf18ea5ca487b769a7f7c1ee1b6cf873f87ce3
KvashninAlexander2023 <study_python@mail.ru>
2024-08-24 13:35:13 +0500

    add get request response

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,41 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
+const port = process.env.PORT || 5000;
+const products = [{ id: 1, title: "tomato" }, { id: 2, title: "orange" }];
+const addresses = [{ id: 1, value: "new 12" }, { id: 2, value: "old 21" }];
 app.get('/', (req, res) => {
-    res.send('Hello World!22211');
+    res.send("Hello");
+});
+app.get('/products/', (req, res) => {
+    if (req.query.title) {
+        let searchString = req.query.title.toString();
+        res.send(products.filter((p) => p.title.indexOf(searchString) > -1));
+    }
+    else {
+        res.send(products);
+    }
+});
+app.get('/products/:id', (req, res) => {
+    let product = products.find((p) => p.id === +req.params.id);
+    if (product) {
+        res.send(product);
+    }
+    else {
+        res.send(404);
+    }
+});
+app.get('/addresses', (req, res) => {
+    res.send(addresses);
+});
+app.get('/addresses/:id', (req, res) => {
+    let address = addresses.find((p) => p.id === +req.params.id);
+    if (address) {
+        res.send(address);
+    }
+    else {
+        res.send(404);
+    }
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,47 @@ import express, {Request, Response } from "express"
 // const express = require('express')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
+
+const products = [{id:1, title:"tomato"},{id:2, title:"orange"}]
+const addresses = [{id:1, value:"new 12"},  {id:2, value:"old 21"}]
 
 app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!22211')
+  res.send("Hello")
+})
+
+app.get('/products/', (req:Request, res:Response) => {
+  if(req.query.title){
+    let searchString = req.query.title.toString()
+    res.send(products.filter((p)=> p.title.indexOf(searchString) > -1))
+  } else {
+    res.send(products)
+  }
+
+})
+
+app.get('/products/:id', (req:Request, res:Response) => {
+  //req.params.id - параметр который мы получаем при запросе
+  let product = products.find((p)=>p.id === +req.params.id)
+  if(product){
+    res.send(product)
+  } else {
+    res.send(404)
+  }
+
+})
+
+app.get('/addresses', (req:Request, res:Response) => {
+  res.send(addresses)
+})
+
+app.get('/addresses/:id', (req:Request, res:Response) => {
+  let address = addresses.find((p)=> p.id === +req.params.id)
+  if(address){
+    res.send(address)
+  } else {
+    res.send(404)
+  }
 })
 
 app.listen(port, () => {
@@ -19,3 +56,4 @@ app.listen(port, () => {
 // yarn add typescript ts-node @types/node @types/express --dev
 // yarn tsc --init
 //yarn tsc -w - режим вотчера(смотрящего - следящего за изменениями)
+// запустить приложение на двух терминалах 1 - yarn dev 2 - yarn watch
